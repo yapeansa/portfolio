c5c0ee3e6a6170b0d874dbdd970454566b7951ba
Yago <yago.pereira@estudante.ufjf.br>
2023-03-04 15:06:13 -0300

    Botão retornar ao topo.

--- js/retornoTopo.js
+++ js/retornoTopo.js
@@ -4,7 +4,7 @@ const altura = screen.availHeight;
 document.addEventListener('scroll', function() {
     if (document.documentElement.scrollTop > altura) {
         clearInterval(window.scrollEndTimer);
-        window.scrollEndTimer = setTimeout(aparecer, 100);
+        window.scrollEndTimer = setTimeout(aparecer, 300);
     } else {
         botao.classList.add('fade-out');
         setTimeout(removeClasse, 300);
@@ -12,14 +12,8 @@ document.addEventListener('scroll', function() {
     }
 });
 
-function aparecer() {
-    botao.style.display = 'block';
-}
+function aparecer() { botao.style.display = 'block'; }
 
-function removeClasse() {
-    botao.classList.remove('fade-out');
-}
+function removeClasse() { botao.classList.remove('fade-out'); }
 
-function desaparecer() {
-    botao.style.display = 'none';
-}
+function desaparecer() { botao.style.display = 'none'; }
