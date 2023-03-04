8049bbf4ecd1328271a10784cad5fabf3a47771d
Yago <yago.pereira@estudante.ufjf.br>
2023-03-04 15:14:05 -0300

    Limpando javascript do botão retornar ao topo.

--- js/retornoTopo.js
+++ js/retornoTopo.js
@@ -1,19 +1,24 @@
 const botao = document.getElementById('to-top');
 const altura = screen.availHeight;
 
-document.addEventListener('scroll', function() {
+document.addEventListener('scroll', () => {
     if (document.documentElement.scrollTop > altura) {
         clearInterval(window.scrollEndTimer);
         window.scrollEndTimer = setTimeout(aparecer, 300);
     } else {
-        botao.classList.add('fade-out');
-        setTimeout(removeClasse, 300);
-        setTimeout(desaparecer, 300);
+        clearInterval(window.scrollEndTimer);
+        window.scrollEndTimer = setTimeout(efeito, 300);
     }
 });
 
-function aparecer() { botao.style.display = 'block'; }
+const aparecer = () => botao.style.display = 'block';
 
-function removeClasse() { botao.classList.remove('fade-out'); }
+const removeClasse = () => botao.classList.remove('fade-out');
 
-function desaparecer() { botao.style.display = 'none'; }
+const desaparecer = () => botao.style.display = 'none';
+
+const efeito = () => {
+    botao.classList.add('fade-out');
+    setTimeout(removeClasse, 300);
+    setTimeout(desaparecer, 300);
+}
