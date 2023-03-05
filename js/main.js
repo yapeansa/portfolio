d816578410b891d63c4249117517eb42990af115
Yago <yago.pereira@estudante.ufjf.br>
2023-03-05 07:34:17 -0300

    Projeto parcialmente concluído.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(function() {
+// $(function() {
     
-    // $(window).on('scroll', function() {
-    //     const altura = screen.availHeight - 100;
-    //     if ($(this).scrollTop() > altura) {
-    //         $('.botao__topo').fadeIn('slow');
-    //     }
-    //     else {
-    //         $('.botao__topo').fadeOut('slow');
-    //     }
-    // });
+//     // $(window).on('scroll', function() {
+//     //     const altura = screen.availHeight - 100;
+//     //     if ($(this).scrollTop() > altura) {
+//     //         $('.botao__topo').fadeIn('slow');
+//     //     }
+//     //     else {
+//     //         $('.botao__topo').fadeOut('slow');
+//     //     }
+//     // });
 
-});
+// });
--- js/menuResponsivo.js
+++ js/menuResponsivo.js
@@ -1,9 +1,27 @@
-$(function() {
-    $('.open').on('click', function() {
-        $('.menu__mobile').stop().fadeIn('slow');
-    });
+// $(function() {
+//     // $('.open').on('click', function() {
+//     //     $('.menu__mobile').stop().fadeIn('slow');
+//     // });
 
-    $('.close').on('click', function() {
-        $('.menu__mobile').stop().fadeOut('slow');
-    });
+//     // $('.close').on('click', function() {
+//     //     $('.menu__mobile').stop().fadeOut('slow');
+//     // });
+// });
+
+const open = document.getElementById('open__menu');
+const close = document.getElementById('close__menu');
+const menu = document.getElementById('menu__m');
+
+open.addEventListener('click', () => {
+    menu.style.display = 'block';
+});
+
+function hide() { menu.style.display = 'none'; }
+
+function remClasse() { menu.classList.remove('fade-out'); }
+
+close.addEventListener('click', function() {
+    menu.classList.add('fade-out');
+    setTimeout(remClasse, 1000);
+    setTimeout(hide, 1000);
 });
