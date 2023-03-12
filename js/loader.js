651b5f3dfc7058584e026c33ad583a320d448fa8
Yago <yago.pereira@estudante.ufjf.br>
2023-03-12 17:06:39 -0300

    Projeto finalizado. Inserindo loader dinamicamente.

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,17 @@
+// Criando o container loader
+
+const loaderContainer = document.createElement('div');
+loaderContainer.id = 'loader';
+loaderContainer.innerHTML = `
+    <div class="animation__loader"></div>
+`;
+
+// Inserindo loader container no body
+
+document.body.prepend(loaderContainer);
+
+// Selecionando o loader pelo id e fazendo o conteúdo principal aparecer
+
 const loader = document.querySelector('#loader');
 const main = document.querySelector('#conteudo__principal');
 
