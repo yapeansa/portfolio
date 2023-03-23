11d8a1bc470054edd94370f437d1fa4e9ff4975d
Yago <yago.pereira@estudante.ufjf.br>
2023-03-23 09:07:45 -0300

    Formulário de contato.

--- js/formContato.js
+++ js/formContato.js
@@ -5,20 +5,6 @@ textos.forEach(elemento => {
     elemento.addEventListener('blur', preencherPlaceholder);
 });
 
-function esvaziarPlaceholder () { this.placeholder = ''; }
+function esvaziarPlaceholder () { this.placeholder = '' }
 
-function preencherPlaceholder () {
-    let holder;
-
-    if (this.id === 'nome') {
-        holder = 'seu nome';
-    }
-    else if (this.id === 'email') {
-        holder = 'seu e-mail';
-    }
-    else {
-        holder = 'sua mensagem';
-    }
-
-    this.placeholder = `Digite ${holder} aqui...`;
-}
+function preencherPlaceholder () { this.placeholder = `${this.name} aqui...` }
